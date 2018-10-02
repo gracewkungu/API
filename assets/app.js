9a56da2f054f816f1d0510b7d91bd99837d1f17c
unknown <gracewkungu@gmail.com>
2018-10-02 15:31:21 -0500

    files

---
+++ assets/app.js
@@ -0,0 +1,9 @@
+const currency = ['FB', 'AAPL', 'TSLA', 'GOOG'];
+
+// displaycurrencyInfo function re-renders the HTML to display the appropriate content
+const displaycurrenctyInfo = function () {
+
+    const currency = $(this).attr('data-name');
+    const queryURL = `https://min-api.cryptocompare.com/`;
+     
+}
